754eacd53486612336d94322c5f4f196bae6bb34
Deepak <chdeepak@thoughtworks.com>
2016-09-13 15:07:52 +0530

    Deepak - Added trigger event code

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = require('http').createServer(handler),
   parser = new require('xml2json'),
   fs = require('fs');
 
+var url = require('url');
+var socketGlobal;
+
 // creating the server ( localhost:8000 )
 app.listen(8000);
 
@@ -10,30 +13,51 @@ console.log('server listening on localhost:8000');
 
 // on server started we can load our client.html page
 function handler(req, res) {
-  fs.readFile(__dirname + '/client.html', function(err, data) {
-    if (err) {
-      console.log(err);
-      res.writeHead(500);
-      return res.end('Error loading client.html');
+
+// your normal server code
+    var path = url.parse(req.url).pathname;
+    switch (path){
+        case '/':
+        default:
+            initialLoad(req,res);
+            break;
+        case '/next':
+            triggerNextQuestion(req,res);
+            res.writeHead(200);
+            res.end('Request Completed');
+            break;
     }
-    res.writeHead(200);
-    res.end(data);
-  });
+}
+
+function initialLoad (req,res){
+       fs.readFile(__dirname + '/client.html', function(err, data) {
+           if (err){
+                 console.log(err);
+                 res.writeHead(500);
+                 return res.end('Error loading client.html');
+           }
+                res.writeHead(200);
+                res.end(data);
+           });
+}
+
+function triggerNextQuestion (req,res){
+    fs.readFile(__dirname + '/example.xml', function(err, data) {
+          if (err) throw err;
+        // parsing the new xml data and converting them into json file
+        var json = parser.toJson(data);
+        // send the new data to the client
+        socketGlobal.volatile.emit('notification', json);
+    });
 }
 
 // creating a new websocket to keep the content updated without any AJAX request
 io.sockets.on('connection', function(socket) {
-  console.log(__dirname);
-  // watching the xml file
-  fs.watchFile(__dirname + '/example.xml', function(curr, prev) {
-    // on file change we can read the new xml
-    fs.readFile(__dirname + '/example.xml', function(err, data) {
-      if (err) throw err;
-      // parsing the new xml data and converting them into json file
-      var json = parser.toJson(data);
-      // send the new data to the client
-      socket.volatile.emit('notification', json);
-    });
-  });
 
+    console.log(__dirname);
+    socketGlobal= socket;
+
+    socket.on('reset', function(data){
+            console.log('reset the session');
+    });
 });

af94510dc536865afdb3df6f0b1eb4a52315e111
Deepak <chdeepak@thoughtworks.com>
2016-09-22 16:45:08 +0530

    for other button led

--- switch.py
+++ switch.py
@@ -5,16 +5,15 @@ import requests
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 GPIO.setup(2, GPIO.OUT)
 
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.OUT)
 
-print('switching off pin 3')
 GPIO.output(2,GPIO.LOW)
+GPIO.output(26,GPIO.LOW)
 
+blink_time =0.8
 
 while True:
     input_state = GPIO.input(18)
@@ -22,12 +21,13 @@ while True:
 		resp = requests.get('http://localhost:8000/submitAnswer/?optionId=0')
 		print('Button Pressed 0. Lighting LED')
 		GPIO.output(2, GPIO.HIGH)
-		time.sleep(1)
+		time.sleep(blink_time)
 		print "LED off"
 		GPIO.output(2,GPIO.LOW)
     input_state_2 = GPIO.input(12)
     if input_state_2 == False:
 		resp_2 = requests.get('http://localhost:8000/submitAnswer/?optionId=1')
 		print('Button Pressed 1')
-		GPIO.output(17, 3)
-		time.sleep(1)
+		GPIO.output(26, GPIO.HIGH)
+		time.sleep(blink_time)
+        GPIO.output(26, GPIO.LOW)

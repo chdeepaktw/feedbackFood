892f54cffd6fb7c7f1930460b7b3c64ab79d5ee2
Deepak <chdeepak@thoughtworks.com>
2016-09-22 16:09:37 +0530

    added gpio on 3

--- switch.py
+++ switch.py
@@ -8,7 +8,7 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(1, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
 
 GPIO.setup(17, GPIO.OUT)
 
@@ -18,7 +18,7 @@ while True:
     if input_state == False:
 		resp = requests.get('http://localhost:8000/submitAnswer/?optionId=0')
 		print('Button Pressed 0. Lighting LED')
-		GPIO.output(1, 3)
+		GPIO.output(3, 3)
 		time.sleep(1)
     input_state_2 = GPIO.input(12)
     if input_state_2 == False:

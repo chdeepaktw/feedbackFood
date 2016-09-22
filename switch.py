cb0b4ee4f3eb25cc3310af8cd909c98a448e87ad
Deepak <chdeepak@thoughtworks.com>
2016-09-22 16:31:55 +0530

    changed pin

--- switch.py
+++ switch.py
@@ -8,7 +8,7 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
 
 GPIO.setup(17, GPIO.OUT)
 
@@ -21,10 +21,10 @@ while True:
     if input_state == False:
 		resp = requests.get('http://localhost:8000/submitAnswer/?optionId=0')
 		print('Button Pressed 0. Lighting LED')
-		GPIO.output(3, GPIO.HIGH)
+		GPIO.output(2, GPIO.HIGH)
 		time.sleep(1)
 		print "LED off"
-		GPIO.output(3,GPIO.LOW)
+		GPIO.output(2,GPIO.LOW)
     input_state_2 = GPIO.input(12)
     if input_state_2 == False:
 		resp_2 = requests.get('http://localhost:8000/submitAnswer/?optionId=1')

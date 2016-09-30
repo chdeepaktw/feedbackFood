cc72297edb4e17ff30320f57cb8504fdcfc22f7f
Deepak <chdeepak@thoughtworks.com>
2016-09-30 09:51:57 +0530

    end message

--- client/socket_communication.js
+++ client/socket_communication.js
@@ -11,6 +11,8 @@ socket.on('notification', function (data) {
   document.getElementById("timer").style.visibility = "visible";
 
   $('#welcome').hide();
+  document.getElementById("endBadge").style.visibility = "hidden";
+
   $('#leftSection').show();
   $('#rightSection').show();
   $('#question').html(_data.question);
@@ -19,6 +21,7 @@ socket.on('notification', function (data) {
 
   $('#optionA').html(_data.options[0]);
   document.getElementById("optionBIcon").style.visibility = "visible";
+document.getElementById("optionAIcon").style.visibility = "visible";
 
   $('#optionB').html(_data.options[1]);
   $('#currentQuestionNumber').html(_data.questionCount);
@@ -64,9 +67,8 @@ socket.on('end', function (data) {
   // convert the json string into a valid javascript object
   var _data = JSON.parse(data);
   console.log(_data);
-  $('#score').html(_data);
-  $('#question').html("");
-  $("#options").hide();
+  setBadge(_data);
+  showResult();
 });
 
 socket.on('welcome', function (data) {
@@ -100,6 +102,33 @@ var setLevel = function (score) {
   }
 };
 
+var setBadge = function (score) {
+  console.log('score ' + score);
+  switch (score) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      document.getElementById("badge-image").src = './images/novice.png';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      document.getElementById("badge-image").src = './images/pro.png';
+      break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      document.getElementById("badge-image").src = './images/expert.png';
+      break;
+    default:
+      document.getElementById("badge-image").src = './images/novice.png';
+      break;
+  }
+};
+
+
 var timer ;
 var setTimer = function(){
 
@@ -129,7 +158,14 @@ var showWelcome = function (){
 }
 
 var showResult = function (){
+  document.getElementById("endBadge").style.visibility = "visible";
+  document.getElementById("correctAnswerGyan").style.visibility = "hidden";
+  document.getElementById("wrongAnswerGyan").style.visibility = "hidden";
+  document.getElementById("timeUpMessage").style.visibility = "hidden";
+
   $('#question').html("");
   $("#options").hide();
-
+     $('#leftSection').hide();
+  $('#rightSection').hide();
+      $('#timer').hide();
 }
